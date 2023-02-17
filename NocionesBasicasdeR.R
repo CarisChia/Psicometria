0ca408d22a83051e3c04f30b1485b0e5ccdf5e94
Jusearchi95 <125619530+Jusearchi95@users.noreply.github.com>
2023-02-16 20:54:08 -0500

    Se agregan disposiciones básicas

--- NocionesBasicasdeR.R
+++ NocionesBasicasdeR.R
@@ -1,7 +1,27 @@
-# Incio del codigo
-# Aqui se debe especificar el para qué se creo y el que hace el código
-# tmabién es bueno si se programa colaborativo poner fecha de creacion autor y fechas de modificacion y comentarios
-# importante Procurar NO poner tildes o  ~n e~nes
+# Incio del codigo.
+# Aqui se debe especificar el para qué se creo y el que hace el código.
+# tmabién es bueno si se programa colaborativo poner fecha de creacion autor y-
+# fechas de modificacion y comentarios.
+# importante Procurar NO poner tildes o  ~n e~nes.
+# se prefiere que ninguna linea de codigo pase los 80 caracteres.
+
+############################ dependiendo del editor de codigo se pueden seccionar el codigo 
 
 rm(list = ls(all.names= TRUE)
 gc()
+
+############################ paths
+   
+############################ carga funciones
+   
+############################ definiciones globales
+   
+   
+############################ Procesamientos
+   
+   
+   
+############################ outputs
+
+   
+   
