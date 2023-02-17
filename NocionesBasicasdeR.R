a64e60cc80de890ea88d9adba7fffbbc81d8900d
Jusearchi95 <125619530+Jusearchi95@users.noreply.github.com>
2023-02-16 20:46:44 -0500

    Archivo para iniciar un Código

---
+++ NocionesBasicasdeR.R
@@ -0,0 +1,7 @@
+# Incio del codigo
+# Aqui se debe especificar el para qué se creo y el que hace el código
+# tmabién es bueno si se programa colaborativo poner fecha de creacion autor y fechas de modificacion y comentarios
+# importante Procurar NO poner tildes o  ~n e~nes
+
+rm(list = ls(all.names= TRUE)
+gc()
